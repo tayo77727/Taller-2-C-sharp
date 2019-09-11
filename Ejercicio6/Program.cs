7b53a8a0b3a0f02690211468d7f3c3f11a9a1acf
tayo77727 <54143373+tayo77727@users.noreply.github.com>
2019-09-10 20:11:36 -0500

    Add files via upload

--- Ejercicio6/Program.cs
+++ Ejercicio6/Program.cs
@@ -14,9 +14,18 @@ namespace Ejercicio6
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("");
+			double F, N, var1, var2, var3;
 			
+			Console.Write("Digite el valor de N: ");
+			N = double.Parse(Console.ReadLine());
 			
+			var1 = 1/Math.Sqrt(5);
+			var2 = (1+Math.Sqrt(5))/2;
+			var3 = (1-Math.Sqrt(5))/2;
+			
+			F = var1*((Math.Pow(var2,N)-Math.Pow(var3,N)));
+			
+			Console.Write("El valor de la expresion es: "+F);
 			
 			Console.Write("\nPress any key to continue . . . ");
 			Console.ReadKey(true);
